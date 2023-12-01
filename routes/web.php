6b93afb8801515bd61c8e0b9c3100a0df531b73f
Zenon <zenropru@gmail.com>
2023-12-01 16:36:04 +0100

    Canvi de rutes
    Nomes amb la ruta de resource

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ColorController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get("/colors", function(){
-    return view('colors');
-});
-Route::get('colors', [ColorController::class, "index"]);
 
 Route::resource('colors', ColorController::class);
-
-Route::get('/colors/{id}', 'ColorController@show')->name('colors.show');
